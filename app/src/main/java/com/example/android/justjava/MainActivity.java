36e514ffafefad66eaec7eb0c02e6b40218c16c7
CyfrowySledz <digitalherrings@gmail.com>
2017-01-31 18:36:32 +0100

    + and - buttons added

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view){
-        int quantity=3;
+        int quantity=2;
         display(quantity);
         displayPrice(quantity*5);
     }
@@ -29,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
     public void displayPrice(int number) {
         TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment (View view){
+        int quantity=3;
+        display(quantity);
+    }
+
+    public void decrement (View view) {
+        int quantity=1;
+        display(quantity);
     }
 }
